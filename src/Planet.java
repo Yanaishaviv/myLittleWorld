878242f31d40a9e1e127c1d75d885c9b41590be2
yanai <yanaishavivra@gmail.com>
2020-08-12 04:06:03 +0300

    adding the spaceship and basic commands

--- src/Planet.java
+++ src/Planet.java
@@ -5,5 +5,25 @@ public class Planet {
     public double mass = 10;
     public double xLoc = 0;
     public double yLoc = 0;
+
+    public Planet(double radius, double mass, double xLoc, double yLoc) {
+        this.radius = radius;
+        this.mass = mass;
+        this.yLoc = yLoc;
+        this.xLoc = xLoc;
+    }
     
+    public Planet(){
+        
+    }
+
+    public double distance(Planet sec) {
+        double x = this.xLoc - sec.xLoc;
+        double y = this.yLoc - sec.yLoc;
+        x *= x;
+        y *= y;
+        return Math.sqrt(x+y);
+    }
+
+    public 
 }
